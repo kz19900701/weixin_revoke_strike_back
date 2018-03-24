5a1c903312688f47ee9eaf75cc3b83bac1982f98
zhangjiekun <46691235@qq.com>
2018-03-24 11:45:59 +0800

    第二版
    支持图片表情，语音的撤回反击

--- lizhiying.py
+++ lizhiying.py
@@ -6,7 +6,7 @@ from itchat.content import *
 import collections
 from html.parser import HTMLParser
 from xml.etree import ElementTree as et
-import time
+import time,json,os,sys
 
 """
     思路：
@@ -42,45 +42,66 @@ def Save_msg(msg):
     tmp_dict['MsgId']= msg['MsgId']
     tmp_dict['Type'] = msg['Type']
     tmp_dict['Text'] = msg['Text']
-    tmp_dict['FromUserName'] = msg['FromUserName']
     tmp_dict['MsgId'] = msg['MsgId']
+    tmp_dict['FileName'] = msg['FileName']
+    tmp_dict['Content'] = msg['Content']
 
     msg_store[CreateTime] = tmp_dict
     # 查看并清理
     clear_data(timeout)
+@bot.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+def download_files(msg):
+    msg["Text"](msg['FileName'])
+    print(type(msg['FileName']))
+    print(msg['FileName'])
+    #msg.download(msg['FileName'])   #这个同样是下载文件的方式
+    #将下载的文件发送给发送者
+    # itchat.send('@%s@%s' % ('img' if msg['Type'] == 'Picture' else 'fil', msg["FileName"]), msg["FromUserName"])
+
+def return_text_and_fn(content):
+    root = et.fromstring(content)
+    if root.tag == 'sysmsg':
+        for child in root:
+            if child.tag == 'revokemsg':
+                for c in child:
+                    if c.tag == 'msgid':
+                        revokemsg_id = int(c.text)
+                        for time, item in msg_store.items():
+                            if int(item['MsgId']) == revokemsg_id:
+                                revoke_msg = msg_store[time]
+                                return revoke_msg
 
 @bot.msg_register([TEXT,NOTE,MAP,CARD,SHARING,PICTURE,RECORDING,ATTACHMENT,VIDEO,SYSTEM],isFriendChat=True)
 def text_reply(msg):
     Save_msg(msg)
-    print(msg)
-    print(type(msg))
-    print(msg_store)
-    print("====")
     # 处理撤回的消息
-    if msg['Type']=='Note':
+    if msg['Type']=='Note' and '撤回' in msg['Text']:
         content = msg['Content']
-        revokemsg_id = 0
-        root = et.fromstring(content)
-        if root.tag == 'sysmsg':
-            for child in root:
-                if child.tag == 'revokemsg':
-                    for c in child:
-                        if c.tag == 'msgid':
-                            revokemsg_id = int(c.text)
-        revokemsg_text = ""
-        revokemsg_toUserNme = ""
-        for time,item in msg_store.items():
-                if int(item['MsgId']) == revokemsg_id:
-                    revokemsg_text = msg_store[time]['Text']
-                    revokemsg_toUserNme = msg_store[time]['FromUserName']
+        revoke_msg = return_text_and_fn(content)
+        orginal_text = msg['Text'] +"\n"+"类型："+revoke_msg['Type']+"\n"
+        if revoke_msg['Type']=='Text':
+            bot.send(orginal_text+"内容："+revoke_msg['Text'],toUserName=msg['FromUserName'])
+            bot.send(orginal_text + "内容：" + revoke_msg['Text'], toUserName='filehelper')
+        elif revoke_msg['Type']=="Picture":
+            fn = data_path+"/img/"+revoke_msg['FileName']
+            revoke_msg['Text'](fn)
+            bot.send('@msg@'+orginal_text.strip(), toUserName=msg['FromUserName'])
+            bot.send('@img@' + fn, toUserName=msg['FromUserName'])
+        elif revoke_msg['Type']=="Recording":
+            fn = data_path+"/recording/"+revoke_msg['FileName']
+            revoke_msg['Text'](fn)
+            bot.send('@msg@'+orginal_text.strip(), toUserName=msg['FromUserName'])
+            bot.send('@fil@' + fn, toUserName=msg['FromUserName'])
 
-        revokemsg_MSG = msg['Text']+"\n" +"内容："+revokemsg_text +"\n"
-        print(revokemsg_MSG)
-        bot.send(msg='@msg@'+revokemsg_MSG,toUserName=revokemsg_toUserNme)
+
 
 
 if __name__ == "__main__":
     if not os.path.exists(data_path):
         os.mkdir(data_path)
+        os.mkdir(data_path+"/file")
+        os.mkdir(data_path+"/img")
+        os.mkdir(data_path + "/vieo")
+        os.mkdir(data_path + "/recording")
 
     bot.run()
